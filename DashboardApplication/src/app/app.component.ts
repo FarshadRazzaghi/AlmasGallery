df3d79f2ba1305cde5e7d8e12f69445fe1786e30
Farshad Abdoulrazaghi <f.razaghi22@gmail.com>
2025-01-17 14:10:19 +0330

    ** ng update @angular/cli **

--- DashboardApplication/src/app/app.component.ts
+++ DashboardApplication/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, ViewEncapsulation, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgComponentOutlet } from '@angular/common';
 
 import { FrThemeService } from '@fr-theme/common';
 import { FrLocalizationService } from '@fr-widget/i18n';
@@ -11,7 +10,7 @@ import { LocalizationService } from './services/localization.service';
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [RouterOutlet, NgComponentOutlet],
+  imports: [RouterOutlet],
   encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
